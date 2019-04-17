0acb31d9ae8cab7774c949306a880e464bc03bb0
Bomnamul <wdv1235@gmail.com>
2019-04-17 21:27:05 +0900

    H/W: CircularQueue Complete

--- Homework/37.CircularQueue/Program.cs
+++ Homework/37.CircularQueue/Program.cs
@@ -20,8 +20,8 @@ namespace _37.CircularQueue
             cQueue.Enqueue(80);
             cQueue.Enqueue(90);
             cQueue.Enqueue(100);
-            // cQueue.Enqueue(110);
-            print(cQueue.Count() == 10);
+            cQueue.Enqueue(110);
+            print(cQueue.Count() == 9);
             print(cQueue.Dequeue() == 10);
             print(cQueue.Dequeue() == 20);
             print(cQueue.Dequeue() == 30);
@@ -31,36 +31,42 @@ namespace _37.CircularQueue
             print(cQueue.Dequeue() == 70);
             print(cQueue.Dequeue() == 80);
             print(cQueue.Dequeue() == 90);
-            print(cQueue.Dequeue() == 100);
-            // print(cQueue.Dequeue());
+            print(cQueue.Count() == 0);
+            print(cQueue.Dequeue() == 0);
             print(cQueue.Count() == 0);
         }
     }
     public class CQueue<T>
     {
-        int head = 0;
-        int tail = 0;
+        int front;
+        int rear;
         int count = 0;
         T[] arr;
         public CQueue(int size)
         {
-            head = 0;
-            tail = 0;
+            front = 0;
+            rear = 0;
             arr = new T[size];
         }
         public void Enqueue(T t)
         {
-            arr[tail] = t;
-            tail++;
+            if ((rear + 1) % arr.Length == front)
+            {
+                Console.WriteLine("Queue is full");
+                return;
+            }
+            arr[rear] = t;
+            rear = (rear % arr.Length) + 1;
             count++;
         }
         public T Dequeue()
         {
-            T temp = arr[head];
-            for (int i = 1; i < arr.Length; i++)
+            if (front == rear)
             {
-                arr[i - 1] = arr[i];
+                return default(T);
             }
+            T temp = arr[front];
+            front = (front % arr.Length) + 1;
             count--;
             return temp;
         }
