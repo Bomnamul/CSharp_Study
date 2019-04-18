c57294ab8bff5824868ffdf279deed38ae03bdb8
Bomnamul <wdv1235@gmail.com>
2019-04-18 23:05:32 +0900

    H/W: BresenhamAlgo
    In making

--- Homework/39.BresenhamAlgo/Program.cs
+++ Homework/39.BresenhamAlgo/Program.cs
@@ -24,7 +24,7 @@ namespace _39.BresenhamAlgo
             };
 
             var line = new LineDrawing(grid);
-            line.Draw(new Node(3, 3), new Node(7, 7));
+            line.Draw(new Node(2, 2), new Node(7, 7));
             print(line.lineGrid[0].Stringify());
             print(line.lineGrid[1].Stringify());
             print(line.lineGrid[2].Stringify());
@@ -35,29 +35,54 @@ namespace _39.BresenhamAlgo
             print(line.lineGrid[7].Stringify());
             print(line.lineGrid[8].Stringify());
             print(line.lineGrid[9].Stringify());
+            print("");
+            print(line.lineGrid[0].Stringify() == " 0  0  0  0  0  0  0  0  0  0");
+            print(line.lineGrid[1].Stringify() == " 0  0  0  0  0  0  0  0  0  0");
+            print(line.lineGrid[2].Stringify() == " 0  0  1  0  0  0  0  0  0  0");
+            print(line.lineGrid[3].Stringify() == " 0  0  0  1  0  0  0  0  0  0");
+            print(line.lineGrid[4].Stringify() == " 0  0  0  0  1  0  0  0  0  0");
+            print(line.lineGrid[5].Stringify() == " 0  0  0  0  0  1  0  0  0  0");
+            print(line.lineGrid[6].Stringify() == " 0  0  0  0  0  0  1  0  0  0");
+            print(line.lineGrid[7].Stringify() == " 0  0  0  0  0  0  0  1  0  0");
+            print(line.lineGrid[8].Stringify() == " 0  0  0  0  0  0  0  0  0  0");
+            print(line.lineGrid[9].Stringify() == " 0  0  0  0  0  0  0  0  0  0");
         }
     }
     public class LineDrawing
     {
         byte[][] grid;
-        public byte[] lineGrid;
+        public byte[][] lineGrid;
         public LineDrawing(byte[][] grid)
         {
             this.grid = grid;
+            this.lineGrid = new byte[grid.Length][];
+            for (int i = 0; i < grid.Length; i++)
+            {
+                this.lineGrid[i] = new byte[grid[0].Length];
+            }
         }
         public void Draw(Node start, Node end)
         {
 
         }
+        public double Solver(Node start, Node end)
+        {
+            int w = end.Y - start.Y;
+            int h = end.X - start.X;
+            double b = start.X - ((h / w) * start.Y);
+            double y = ((h / w) * x) + b;
+
+            return y;
+        }
     }
     public class Node
     {
-        public int row;
-        public int column;
-        public Node(int row, int column)
+        public int X {get; set;}
+        public int Y {get; set;}
+        public Node(int x, int y)
         {
-            this.row = row;
-            this.column = column;
+            X = x;
+            Y = y;
         }
     }
     public static class ClassExtension
